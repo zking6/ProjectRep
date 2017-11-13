c424ad7068bee431c57550bf36b07458f7c5aadd
Tristan Allan <tallan@uncc.edu>
2017-11-13 21:26:00 +0000

    Updated Character controller with CRUD functions

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,13 +1,61 @@
 class CharactersController < ApplicationController
-  def name:string
-  end
+  #variables and types
+  #def name:string
+  #end
 
-  def background:text
-  end
+  #def background:text
+  #end
 
-  def race:string
-  end
+  #def race:string
+  #end
 
-  def classes:string
+  #def classes:string
+  #end
+  def index
+    @characters = Character.all
+  end
+    
+  def show
+    @character = Character.find(params[:id])
+  end
+  
+  def new
+    @character =Character.new(character_params)
+  end
+  
+  def create
+    @character = Character.new(character_params)
+
+    if @character.save
+      redirect_to @character
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @character = Character.find(params[:id])
+  end
+  
+  def update
+    @character = Character.find(params[:id])
+    
+    if @character.update(character_params)
+      redirect_to @character
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+
+    redirect_to characters_path
   end
 end
+
+private 
+  def character_params
+    params.require(:character).permit(:name, :background, :race, :class)
+  end
