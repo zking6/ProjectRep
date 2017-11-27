3601e5719f02555f1650eee11c807364166c9153
aashfor1 <aashfor1@uncc.edu>
2017-11-27 21:33:28 +0000

    Updated druid, fight and monk controllers.

--- app/controllers/druids_controller.rb
+++ app/controllers/druids_controller.rb
@@ -1,2 +1,45 @@
-class DruidsController < ApplicationController
+class DruidsController < CharactersController
+  def index
+    @druids = Druid.all
+  end
+    
+  def show
+    @druids = Druid.find(params[:id])
+  end
+  
+  def create
+    @druid = Druid.new(druid_params)
+
+    if @druid.save
+      redirect_to @druid
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @druid = Druid.find(params[:id])
+  end
+  
+  def update
+    @druid = Druid.find(params[:id])
+    
+    if @druid.update(druid_params)
+      redirect_to @druid
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @druid = Druid.find(params[:id])
+    @druid.destroy
+
+    redirect_to druids_path
+  end
 end
+
+private 
+  def druid_params
+    params.require(:druid).permit(:strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :armorclass, :gold)
+  end
--- app/controllers/monks_controller.rb
+++ app/controllers/monks_controller.rb
@@ -1,2 +1,45 @@
-class MonksController < ApplicationController
+class MonksController < CharactersController
+  def index
+    @Monks = Monk.all
+  end
+    
+  def show
+    @Monks = Monk.find(params[:id])
+  end
+  
+  def create
+    @Monk = Monk.new(Monk_params)
+
+    if @Monk.save
+      redirect_to @Monk
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @Monk = Monk.find(params[:id])
+  end
+  
+  def update
+    @Monk = Monk.find(params[:id])
+    
+    if @Monk.update(Monk_params)
+      redirect_to @Monk
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @Monk = Monk.find(params[:id])
+    @Monk.destroy
+
+    redirect_to Monks_path
+  end
 end
+
+private 
+  def Monk_params
+    params.require(:Monk).permit(:strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :armorclass, :gold)
+  end
