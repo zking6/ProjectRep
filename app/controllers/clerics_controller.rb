06a8041d1a6c46345add6d0aa52a0384741d5a3e
aashfor1 <aashfor1@uncc.edu>
2017-11-27 21:22:43 +0000

    Updated cleric and bard controllers.

--- app/controllers/clerics_controller.rb
+++ app/controllers/clerics_controller.rb
@@ -1,2 +1,45 @@
-class ClericsController < ApplicationController
+class ClericsController < CharactersController
+  def index
+    @clerics = Cleric.all
+  end
+    
+  def show
+    @clerics = Cleric.find(params[:id])
+  end
+  
+  def create
+    @cleric = Cleric.new(cleric_params)
+
+    if @cleric.save
+      redirect_to @cleric
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @cleric = Cleric.find(params[:id])
+  end
+  
+  def update
+    @cleric = Cleric.find(params[:id])
+    
+    if @cleric.update(cleric_params)
+      redirect_to @cleric
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @cleric = Cleric.find(params[:id])
+    @cleric.destroy
+
+    redirect_to clerics_path
+  end
 end
+
+private 
+  def cleric_params
+    params.require(:cleric).permit(:strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :armorclass, :gold)
+  end
