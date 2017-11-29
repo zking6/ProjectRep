1dabf19cd54586b4a2e1acc0d8845ea58ba134be
aashfor1 <aashfor1@uncc.edu>
2017-11-29 20:53:07 +0000

    Updated wizards controller.

--- app/controllers/wizards_controller.rb
+++ app/controllers/wizards_controller.rb
@@ -1,2 +1,46 @@
-class WizardsController < ApplicationController
+class WizardsController < CharactersController
+  def index
+    @wizards = Wizard.all
+  end
+    
+  def show
+    @wizards = Wizard.find(params[:id])
+  end
+  
+  def create
+    @wizard = Wizard.new(Wizard_params)
+
+    if @wizard.save
+      redirect_to @wizard
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @wizard = Wizard.find(params[:id])
+  end
+  
+  def update
+    @wizard = Wizard.find(params[:id])
+    
+    if @wizard.update(Wizard_params)
+      redirect_to @wizard
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @wizard = Wizard.find(params[:id])
+    @wizard.destroy
+
+    redirect_to wizards_path
+  end
 end
+
+private 
+  def wizard_params
+    params.require(:wizard).permit(:strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :armorclass, :gold)
+  end
+
