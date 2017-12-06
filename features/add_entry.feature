0fa56ec88bc7a166ec90ce1f526a71a022e61adb
James Heuser <JHeuser@uncc.edu>
2017-12-06 21:13:23 +0000

    testing 1 pass

--- features/add_entry.feature
+++ features/add_entry.feature
@@ -1,3 +1,8 @@
 Feature: Creating a Character
   
-  
+    Scenario: As a DM I want to be able to create a new character
+    Given I am on the home page
+    When I press "Submit"
+    Then I should be on the "Characters" page
+    And I should see the "Name" field
+    And I should see the "Backround" field
---
+++ features/step_definitions/Charater_steps.rb
@@ -0,0 +1,15 @@
+Given("I am on the home page") do
+    visit root_path
+end
+
+When("I press {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be on the {string} page") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see the {string} field") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
