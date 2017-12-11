78c413834ea0f083c5569c0bcbf96b87f3fba784
aashfor1 <aashfor1@uncc.edu>
2017-12-11 03:33:42 +0000

    Revamped front end and added front end portion of comments.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  
-  resources :characters do
-    resources :comments
-  end
-  
-get "characters/new" => "characters#new"
-  root 'welcome#index'
- 
- 
-  
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'characters/index'
+    resources :characters do
+      resources :comments
+    end
+  root 'characters#index'
 end
