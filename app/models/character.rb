84479ea4d2deaea90d63a7992ac6e62b8f9b6257
Tristan Allan <tallan@uncc.edu>
2017-12-13 06:13:38 +0000

    Added validation to character model though it is commented out because it stops the cucumber from working.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,5 @@
 class Character < ApplicationRecord
     has_many :comments
+    #validates :name, presence: true, length: { minimum: 3 }
+    #validates_presence_of :background, :message => "It may be a mystery where they came from, but not so much so that there is nothing to put."
 end
